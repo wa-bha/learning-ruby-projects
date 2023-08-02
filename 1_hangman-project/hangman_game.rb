706153e40d34609dceb426f28fd20dd03ec83cfd
wa-bha <bhavit.w04@gmail.com>
2023-08-02 13:55:48 +1200

    refactor: move interal methods as private section of the class

--- 1_hangman-project/hangman_game.rb
+++ 1_hangman-project/hangman_game.rb
@@ -10,58 +10,58 @@ class HangmanGame
         @word.size.times do 
             @word_teaser += "_ "
         end
-    end
 
-    # Update the word_teaser with the last guess
-    def update_word_teaser last_guess
-        new_teaser = @word_teaser.split
+        # Update the word_teaser with the last guess
+        def update_word_teaser last_guess
+            new_teaser = @word_teaser.split
 
-        new_teaser.each_with_index do |letter, index|
-            if letter == "_" && @word[index] == last_guess
-                new_teaser[index] = last_guess
+            new_teaser.each_with_index do |letter, index|
+                if letter == "_" && @word[index] == last_guess
+                    new_teaser[index] = last_guess
+                end
+            end
+            @word_teaser = new_teaser.join(" ")
+        end
+
+        # Check if the word has been found
+        def check_word_match
+            current_word_guess = @word_teaser.split.join
+
+            if @word == current_word_guess
+                puts
+                puts "You won! 🎉🎉🎉 The word was #{@word}"
+                exit
             end
         end
-        @word_teaser = new_teaser.join(" ")
-    end
 
-    # Check if the word has been found
-    def check_word_match
-        current_word_guess = @word_teaser.split.join
-
-        if @word == current_word_guess
+        # Returns a validated letter guess
+        def get_validated_guess
             puts
-            puts "You won! 🎉🎉🎉 The word was #{@word}"
-            exit
-        end
-    end
+            puts "Enter a new letter to guess:"
+            guess = gets.chomp.upcase
 
-    # Returns a validated letter guess
-    def get_validated_guess
-        puts
-        puts "Enter a new letter to guess:"
-        guess = gets.chomp.upcase
+            # Validate user input is a single valid letter
+            unless guess.match?(/[A-Za-z]/) && guess.length == 1
+                puts "Invalid guess! Please enter a single valid letter."
+                get_guess
+            end
 
-        # Validate user input is a single valid letter
-        unless guess.match?(/[A-Za-z]/) && guess.length == 1
-            puts "Invalid guess! Please enter a single valid letter."
-            get_guess
+            guess
         end
 
-        guess
-    end
+        # Checks if the guessed letter is correct and returns the appropriate message
+        def handle_guess last_guess 
+            is_guess_correct = @word.include? last_guess
 
-    # Checks if the guessed letter is correct and returns the appropriate message
-    def handle_guess last_guess 
-        is_guess_correct = @word.include? last_guess
+            if is_guess_correct
+                puts "You guessed '#{last_guess}' correctly!"
+            else
+                @lives -= 1
+                puts "You've guessed '#{last_guess}' incorrectly, you have #{@lives} lives left."
+            end
 
-        if is_guess_correct
-            puts "You guessed '#{last_guess}' correctly!"
-        else
-            @lives -= 1
-            puts "You've guessed '#{last_guess}' incorrectly, you have #{@lives} lives left."
+            puts @word_teaser
         end
-
-        puts @word_teaser
     end
 
     def start
@@ -74,8 +74,8 @@ class HangmanGame
         while @lives > 0
             guess = get_validated_guess
             update_word_teaser(guess)
+            handle_guess(guess)
             check_word_match
-            handle_guess(guess)        
         end
             
         puts
